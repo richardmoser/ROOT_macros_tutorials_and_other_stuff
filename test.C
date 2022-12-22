9fd830024bb3c11f88c126bd9ea282a5d21444bd
dick <moserrj@ku.edu>
2022-12-21 20:06:10 -0600

    Adding a new file for making a scatterplot of peak powers by frequency. This commit is not useable code (it is a copy of sumpowfreq.C)

--- test.C
+++ test.C
@@ -1,124 +1,232 @@
-#include <iostream>
-#include <TGraph.h>
 
-using namespace std;
+int glob_to_x(TH2* hist, int global){
+    int x_bins = hist->GetNbinsX();
+    int temp = global % (x_bins + 2);
+    // x bin value is global % the total number of x bins (GetNbinsX doesn't count either buffer bin)
+    return temp;
+}
 
-//defines a function which returns a TGraph
-TGraph* makegraph(int t_window, TH2D *spec){
+int glob_to_y(TH2* hist, int global){
+    int x_bins = hist->GetNbinsX();
+    int temp = global / (x_bins + 2);
+    // x bin value is global % the total number of x bins (GetNbinsX doesn't count either buffer bin)
+    return temp;
+}
+double* power_generator(TH2D *spec, double *pows_in, double_t loop_i){
     int NumFreqBins = spec->GetNbinsX();
     int NumIntensBins = spec->GetNbinsY();
     int binmax = spec->GetMaximumBin(); // bin number of max intensity
-    double *pows = new double[NumFreqBins]; // array of frequencies
+    int xmax = glob_to_x(spec, binmax); // x bin number of max intensity
+    double *pows2 = new double[NumFreqBins]; // array of frequencies
+    double *retpows = new double[NumFreqBins]; // array of frequencies
     double *ybin = new double[NumFreqBins]; // array of y bin numbers
     double_t iterator = 0; // iterator for the for loop
-    int i = t_window - 390; // time bin number of the max intensity
+//    int i = binmax - 390; // time bin number of the max intensity
+//    int i = 460 - 390;
+    int i = xmax;
     for(int j = 0; j < spec->GetNbinsY(); j++){
-        if(j==0){pows[j]=0;}
-        else{pows[j]=spec->GetBinContent(i,j);}
-        ybin[j] = j*0.015625*1000; // y bin number x YBinWidth * 1000MHz/GHz
+        if(j==0){
+            pows2[j]=0;
+        }
+        else{
+            pows2[j]=spec->GetBinContent(i,j);
+        }
+//        ybin[j] = j*.00625*1000*2.5; // y bin number x .2GHz/32bins * 1000MHz/GHz * 2.5 to account for overlap
+        ybin[j] = j*.0625*1000*2.5; // y bin number x .2GHz/32bins * 1000MHz/GHz * 2.5 to account for overlap
+
         iterator ++;
     }
-    TGraph *gr = new TGraph(NumFreqBins, ybin, pows);
-    return gr;
+    // loop over each entry in pows, add the value of pows_in, then return pows
+    for (int k = 0; k < NumFreqBins; k++){
+        retpows[k] = pows2[k] + 1*pows_in[k];
+//        cout << "pows[" << k << "] = " << pows[k] << " retpows[" << k << "] = " << retpows[k] << endl;
+    }
+    int intk = loop_i;
+    spec->GetYaxis()->SetRangeUser(0, 5);
+//    spec->GetZaxis()->SetRangeUser(5*pow(10,-17),1*pow(10,-14));
+    spec->SetStats(0); // removes stats box
+
+//    auto c2 = new TCanvas("c2", "c2", 800, 600);
+//    spec->Draw("colz");
+//    string path = "/home/rj/RadioScatter/outputfiles/";
+//    string filename = "spec_wide_" + to_string(intk) + ".png";
+//    string full_path = path + filename;
+//    const char *charpath = full_path.c_str();
+//    c2->SaveAs(charpath);
+//
+//    TH2D *spec2 = (TH2D*)spec->Clone();
+//    auto c3 = new TCanvas("c2", "c2", 800, 600);
+//    spec2->GetYaxis()->SetRangeUser(0, 1);
+//    spec2->Draw("colz");
+//    string path2 = "/home/rj/RadioScatter/outputfiles/";
+//    string filename2 = "spec_narrow_" + to_string(intk) + ".png";
+//    string full_path2 = path + filename2;
+//    const char *charpath2 = full_path2.c_str();
+//    c3->SaveAs(charpath2);
+
+    return retpows;
 }
 
-void doit() {
-    TString infile = "/home/rj/RadioScatter/doc/smallmultiscat_0MHz_10W_10ns.root";
+
+void doit(){
+    TString infile = "/home/rj/RadioScatter/outputfiles/multiscat_0GHz_10W_10ns.root";
+
     auto ff = TFile::Open(infile, "READ");
-
-    TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
-    c1->Divide(2, 2);
-//    c1->SetWindowSize(1200, 1200);
-    c1->SetWindowSize(1200, 800);
-
-//    c1->cd(1)->SetGrid();
-    gPad->SetLeftMargin(.1);
-    gPad->SetBottomMargin(.1);
-    c1->cd(1);
-//    TPad *pad1 = new TPad("pad1", "pad1", 0, 0.3, 1, 1);
-//    pad1->SetBottomMargin(0);
-//    pad1->Draw();
-//    pad1->cd();
-//    TPad *pad2 = new TPad("pad2", "pad2", 0, 0, 1, 0.3);
-//    pad2->SetTopMargin(0);
-    c1->cd(1)->SetLeftMargin(.15);
-    c1->cd(1)->SetRightMargin(.2);
-    c1->cd(2)->SetLeftMargin(.15);
-//    pad2->SetRightMargin(.5);
-//    pad2->SetBottomMargin(0.2);
-//    pad2->Draw();
-//    pad2->cd();
 
     int rxindex = 0;
     int txindex = 0;
     int antenna = 0;
     int entry = 0;
-    auto tree = (TTree *) ff->Get("tree");
+    auto tree=(TTree*)ff->Get("tree");
+
+    TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
+    c1->Divide(3, 2);
+    //    c1->SetWindowSize(1200, 1200);
+    c1->SetWindowSize(2000, 1000);
+    gPad->SetLeftMargin(.1);
+    gPad->SetBottomMargin(0);
+    c1->cd(1);
+//    c1->cd(1)->SetLeftMargin(.15);
+//    c1->cd(1)->SetRightMargin(.2);
+//    c1->cd(2)->SetLeftMargin(.15);
+//    c1->cd(3)->SetLeftMargin(.15);
+//    c1->cd(3)->SetRightMargin(.2);
+//    c1->cd(4)->SetLeftMargin(.15);
+
     auto event = new RadioScatterEvent();
     tree->SetBranchAddress("event", &event);
-    tree->GetEntry(entry);
-// tree->GetEntries();
-// cout << event->nrx;
-//    auto c1 = TUtilRadioScatter::canvas();
-//    c1->Clear();
-    auto evG = event->getGraph(rxindex, txindex);
+
+    int entries = tree->GetEntries();
+    tree->GetEntry(entry); // get the first entry
+    auto evG = event->getGraph(rxindex, txindex); // generates graph evG
     TUtilRadioScatter::titles(evG, "", "Time [ns]", "V");
     TUtilRadioScatter::style(evG, kBlack, 1, 1);
     TUtilRadioScatter::xrange(evG, evG->GetX()[0], evG->GetX()[evG->GetN() - 1]);
     int bins = 32, overlap = 31;
-    auto spec = TUtilRadioScatter::FFT::spectrogram(evG, bins, overlap, bins * 2, 2, 0, 0, .2);
-    c1->SetLogy(0);
-// evG->Draw();
-    c1->cd(1);
-    c1->cd(1)->SetLogy(0);
-    spec->SetStats(0);
-    spec->Draw("colz");
-//    c1->Draw();
+//    int bins = 64, overlap = 62;
 
-    int  time_window = 479;
-    int i = time_window - 390;
+    auto spec = TUtilRadioScatter::FFT::spectrogram(evG, bins, overlap, bins * 2, 2, 0, 0, .2); // generates spectrogram spec
+    c1->SetLogy(0); // set y axis to linear
+    c1->cd(1); //moves to canvas 1, upper left
+    c1->cd(1)->SetLogy(0); // set y axis to linear
+    spec->GetYaxis()->SetRangeUser(0, 0.5);
+    c1->cd(1)->SetLogz(1); // set z axis to log
+//    spec->GetZaxis()->SetRangeUser(5*pow(10,-12),1*pow(10,-10));
+//    spec->GetZaxis()->SetRangeUser(5*pow(10,-17),1*pow(10,-14));
+    spec->SetStats(0); // removes stats box
+    spec->Draw("colz"); // draws spectrogram to canvas 1 upper left
 
-    auto mg = new TMultiGraph();
-    c1->cd(2)->SetLogy(1);
-    int binmax = spec->GetMaximumBin()-390; // bin number of max intensity
-    cout << "max bin number: " << binmax << "ns" << endl; // 390 is the time bin number of the first bin
-    auto gr0 = makegraph(binmax, spec); gr0->SetMarkerStyle(20);
-    auto gr1 = makegraph(binmax-2, spec); gr1->SetMarkerStyle(21);
-    auto gr2 = makegraph(binmax+2, spec); gr2->SetMarkerStyle(22);
-    auto gr3 = makegraph(binmax-4, spec); gr3->SetMarkerStyle(23);
-    auto gr4 = makegraph(binmax+4, spec); gr4->SetMarkerStyle(24);
-    auto gr5 = makegraph(binmax-6, spec); gr5->SetMarkerStyle(25);
-    auto gr6 = makegraph(binmax+6, spec); gr6->SetMarkerStyle(26);
-//    gr1->GetYaxis()->SetRangeUser(1*pow(10,-16.5), 1*pow(10,-11));
+    int  time_window = 479; // time window of interest
+
+    auto mg = new TMultiGraph(); // creates a multigraph
+    c1->cd(2)->SetLogy(1); // set y axis to logarithmic
+
+    int NumFreqBins=spec->GetNbinsX();
+    int NumTimeBins=spec->GetNbinsY();
+    double *pows = new double[NumFreqBins];
+    double *ybin = new double[NumFreqBins];
+    double_t iterator = 0;
+
+    int spec_start_time; // time at which the spectrogram "spec" starts
+    if(fmod(evG->GetX()[0], 1) >=.5){ // C++ rounds down for all numbers, so
+        spec_start_time = evG->GetX()[0] + 1; // if the decimal is >= .5, add 1 to the integer part
+    }
+    else{
+        spec_start_time = evG->GetX()[0]; // otherwise, just use the integer part
+    }
+
+    int binmax = spec->GetMaximumBin(); // bin number of max intensity
+    int xmax = glob_to_x(spec, binmax); // x bin number of max intensity
+    cout << spec->GetNbinsX() << " " << spec->GetNbinsY() << endl;
+    cout << "max, global bin number = " << binmax << endl;
+    cout << "max, x bin number = " << xmax << endl;
+    cout << "start time = " << spec_start_time << endl;
+    cout << "max time = " << spec_start_time + xmax << endl;
+    cout << "NumFreqBins = " << NumFreqBins << endl;
+    cout << "NumTimeBins = " << NumTimeBins << endl;
+
+    int i = xmax;
+    for(int j = 0; j < spec->GetNbinsY(); j++){
+        if(j==0){
+            pows[j]=0;
+        }
+        else{
+            pows[j]=spec->GetBinContent(i,j);
+        }
+        ybin[j] = j*.0625*1000*2.5; // y bin number x .2GHz/32bins * 1000MHz/GHz * 2.5 to account for overlap
+        iterator ++;
+    }
+
+        cout << spec->GetNbinsY()*.00625*1 << endl;
+
+    TGraph *gr = new TGraph(NumFreqBins, ybin, pows);
+//    gr->GetYaxis()->SetRangeUser(1*pow(10,-18), 1*pow(10,-8));
+    gr->GetYaxis()->SetRangeUser(1*pow(10,-18), 5*pow(10,-9));
+//    gr->GetYaxis()->SetLimits(1*pow(10,-18), 1*pow(10,-15));
+    gr->SetMarkerColor(4);
+    gr->SetMarkerSize(.5);
+    gr->SetMarkerStyle(21);
+//    gr->GetXaxis()->SetLimits(0,200);
+    gr->GetXaxis()->SetLimits(0,5000);
+    gr->SetTitle(Form("Frequency vs asdfasdf Power at time %dns", (i + spec_start_time))); // set title of graph with the time of maximum intensity
+    gr->GetXaxis()->SetTitle("Frequency (MHz)");
+    gr->GetYaxis()->SetTitle("Power WGHz^{-1}");
+    double *newpows = new double[NumFreqBins];
+
+    // vvv
+    for(int j = 0; j < spec->GetNbinsY(); j++){
+        newpows[j]=0;
+    }
+    iterator ++;
+
+    for (int k = 0; k < entries -1; k++){
+        tree->GetEntry(k); // get the first entry
+        auto evG = event->getGraph(rxindex, txindex); // generates graph evG
+        TUtilRadioScatter::titles(evG, "", "Time [ns]", "V");
+        TUtilRadioScatter::style(evG, kBlack, 1, 1);
+        TUtilRadioScatter::xrange(evG, evG->GetX()[0], evG->GetX()[evG->GetN() - 1]);
+        int bins = 32, overlap = 31;
+        auto spec2 = TUtilRadioScatter::FFT::spectrogram(evG, bins, overlap, bins * 2, 2, 0, 0, .2); // generates spectrogram spec
+        newpows = power_generator(spec2, newpows, k);
+//        cout << k << endl;
+    }
+
     c1->cd(2);
-    mg->Add(gr0, "ACP");
-    mg->Add(gr1, "ACP");
-    mg->Add(gr2, "ACP");
-    mg->Add(gr3, "ACP");
-    mg->Add(gr4, "ACP");
-    mg->Add(gr5, "ACP");
-    mg->Add(gr6, "ACP");
+    mg->Add(gr, "ACP");
+
     mg->GetXaxis()->SetLimits(0,200);
     mg->GetXaxis()->SetTitle("Frequency (MHz)");
     mg->GetYaxis()->SetTitle("Power (WGHz^{-1} x 10^{-12})");
-    mg->SetTitle(Form("Frequency vs Power at time %dns", 390+i));
+    mg->SetTitle(Form("Frequency vs Power at time %dns on ", 390+i));
 
-    mg->Draw("A pmc plc");
+    //    mg->Draw("ACP");
+    gr->Draw("ACP");
 
     c1->cd(4);
-    TLegend *leg = new TLegend(0.25, 0.6, 0.6, 1);
-    leg->SetFillColor(0);
-    leg->SetHeader("Frequency vs Power\n");
-    leg->AddEntry(gr5, Form("%dns", binmax-6), "p");
-    leg->AddEntry(gr3, Form("%dns", binmax-4), "p");
-    leg->AddEntry(gr1, Form("%dns", binmax-2), "p");
-    leg->AddEntry(gr0, Form("%dns", binmax), "p");
-    leg->AddEntry(gr2, Form("%dns", binmax+2), "p");
-    leg->AddEntry(gr4, Form("%dns", binmax+4), "p");
-    leg->AddEntry(gr6, Form("%dns", binmax+6), "p");
-    leg->Draw();
+    c1->cd(4)->SetLogy(1);
+    TGraph *gr1 = new TGraph(NumFreqBins, ybin, newpows);
+    gr1->GetYaxis()->SetRangeUser(1*pow(10,-18), 1*pow(10,-12));
+    // gr1->GetYaxis()->SetLimits(1*pow(10,-18), 1*pow(10,-15));
+    gr1->GetXaxis()->SetRangeUser(0,500);
+    gr1->GetXaxis()->SetLimits(0,500);
+    gr1->SetMarkerColor(4);
+    gr1->SetMarkerSize(.5);
+    gr1->SetMarkerStyle(21);
+    // gr1->GetXaxis()->SetLimits(0,200);
+    gr1->SetTitle("Frequency vs Power summed over all events");
+    gr1->GetXaxis()->SetTitle("");
+    gr1->GetYaxis()->SetTitle("WGHz^{-1}");
+    gr1->Draw("ACP");
+    cout << gr1->GetMaximum() << endl;
+
+    // spec->GetZaxis()->SetRangeUser(1*pow(10,-18), 1*pow(10,-15));
+    // spec->Draw("colz"); // draws spectrogram to canvas 1 upper left
 
 
-    c1->SaveAs("/home/rj/RadioScatter/outputfiles/powerfreq.png");
+    c1->Draw();
+
+
+
+    //    c1->SaveAs("/home/rj/RadioScatter/outputfiles/sumpowfreq.png");
 
 }
